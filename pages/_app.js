ae5910ce044e7de71461962aaf323bf87e86f5dc
Antonio Pierro <antonio.pierro@gmail.com>
2021-05-05 12:28:57 +0200

    feat(appbridgeconfig): add appBridgeConfig config

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import Head from "next/head";
 import { AppProvider as PolarisProvider, Frame } from "@shopify/polaris";
 import { DesignPopupProvider } from "@contexts/DesignPopup/DesignPopup.context";
 import AlfredLayout from "@components/AlfredLayout/AlfredLayout.component";
+import { ConfigurationProvider } from "@contexts/Configuration/Configuration.context";
+import { UserProvider } from "@contexts/User/User.context";
+import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
+import { isDev } from "@utils/Env.utils";
 
 import { appWithTranslation } from "next-i18next";
 import { useRouter } from "next/dist/client/router";
@@ -11,25 +15,36 @@ const MyApp = ({ Component, pageProps }) => {
     const router = useRouter();
     router.locale = "it";
     const polarisTranslations = require(`@shopify/polaris/locales/${router.locale}.json`);
+    const appBridgeConfig = {
+        apiKey: process.env.SHOPIFY_API_KEY,
+        shopOrigin: isDev() ? process.env.DEV_SHOPIFY_STORE : router.query.shop,
+        forceRedirect: true,
+    };
     return (
         <PolarisProvider
             features={{ newDesignLanguage: true }}
             i18n={polarisTranslations}
         >
-            <Frame>
-                <DesignPopupProvider>
-                    <Head>
-                        <script src="https://alfred-script-tags.s3-eu-west-1.amazonaws.com/cookiebot-popup.js" />
-                    </Head>
+            <AppBridgeProvider config={appBridgeConfig}>
+                <Frame>
+                    <UserProvider>
+                        <ConfigurationProvider>
+                            <DesignPopupProvider>
+                                <Head>
+                                    <script src="https://alfred-script-tags.s3-eu-west-1.amazonaws.com/cookiebot-popup.js" />
+                                </Head>
 
-                    <AlfredLayout
-                        withLanguageSelect={false}
-                        withAnnouncement={false}
-                    >
-                        <Component {...pageProps} />
-                    </AlfredLayout>
-                </DesignPopupProvider>
-            </Frame>
+                                <AlfredLayout
+                                    withLanguageSelect={false}
+                                    withAnnouncement={false}
+                                >
+                                    <Component {...pageProps} />
+                                </AlfredLayout>
+                            </DesignPopupProvider>
+                        </ConfigurationProvider>
+                    </UserProvider>
+                </Frame>
+            </AppBridgeProvider>
         </PolarisProvider>
     );
 };
